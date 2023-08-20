08d343234a66d60f8ab88be952c76d74badfca4f
suzurini <valloforr@gmail.com>
2023-08-20 23:05:25 +0300

    fix: fixed sorted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  const arraySort = concat(matrix[0], matrix[1], matrix[2]);
-  arraySort.sort();
-  return arraySort;
+  const arraySort = matrix.flat();
+  return arraySort.toSorted((a, b) => a - b);
 }
