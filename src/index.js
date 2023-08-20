a1138974d4d65d0b7624a01c714d769f59a87eb1
suzurini <valloforr@gmail.com>
2023-08-20 22:53:14 +0300

    feat: create concat array and sort it

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const arraySort = concat(matrix[0], matrix[1], matrix[2]);
+  arraySort.sort();
+  return arraySort;
 }
