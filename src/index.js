e908acf41eeb49590af56ce5e266005415f7ce71
suzurini <valloforr@gmail.com>
2023-08-20 23:19:40 +0300

    fix: reverse second array

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  const arraySort = matrix.flat();
-  return arraySort.toSorted((a, b) => a - b);
+  matrix[0] = matrix[0].sort((a, b) => a - b);
+  matrix[1] = matrix[1].sort((a, b) => a - b);
+  matrix[2] = matrix[2].sort((a, b) => a - b);
+  matrix = matrix.flat()
+  // const arraySort = matrix.flat();
+  return matrix;
 }
